263356cb08b255ec09d2235ceeb9bcbf375a9376
whjones <whjones@DESKTOP-R0H43D6.attlocal.net>
2022-06-18 16:46:54 -0500

    handle more failure cases

--- src/main.py
+++ src/main.py
@@ -1,16 +1,27 @@
 import importlib
+import os
 import typer
 
 app = typer.Typer()
 
 @app.command()
 def day(day_number: str, use_sample_input=typer.Option(False, "--use-sample-input", "-s", is_flag=True, flag_value=True)):
-  day_solution = importlib.import_module(f"days.{day_number}")
+  solution_path = f"days.{day_number}"
+  try:
+    day_solution = importlib.import_module(solution_path)
+  
+  except ModuleNotFoundError:
+    typer.echo(f"solution for day {day_number} not found")
+    return
 
   if use_sample_input:
     day_input = f"src/days/input/{day_number}-sample.txt"
   else:
     day_input = f"src/days/input/{day_number}.txt"
+
+  if not os.path.exists(day_input):
+    type.echo(f"can't find input file {day_input}")
+    return
 
   try:
     typer.echo(f"part one: {day_solution.part_one(day_input)}")
